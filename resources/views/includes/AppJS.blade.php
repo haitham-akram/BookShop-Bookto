f005c5d4c5119ff3cb5b959ad9fb307163002230
haitham abu lamdi <hayhtam14@gmail.com>
2021-10-21 02:32:35 +0300

    change css and js links

--- resources/views/includes/AppJS.blade.php
+++ resources/views/includes/AppJS.blade.php
@@ -1,50 +1,102 @@
+{{--<!-- Optional JavaScript -->--}}
+{{--<!-- jQuery first, then Popper.js, then Bootstrap JS -->--}}
+{{--<script src="{{asset('js/jquery.min.js')}}"></script>--}}
+{{--<script src="{{asset('js/popper.min.js')}}"></script>--}}
+{{--<script src="{{asset('js/bootstrap.min.js')}}"></script>--}}
+{{--<script src="{{asset('js/jquery.dataTables.min.js')}}"></script>--}}
+{{--<script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>--}}
+{{--<!-- Appear JavaScript -->--}}
+{{--<script src="{{asset('js/jquery.appear.js')}}"></script>--}}
+{{--<!-- Countdown JavaScript -->--}}
+{{--<script src="{{asset('js/countdown.min.js')}}"></script>--}}
+{{--<!-- Counterup JavaScript -->--}}
+{{--<script src="{{asset('js/waypoints.min.js')}}"></script>--}}
+{{--<script src="{{asset('js/jquery.counterup.min.js')}}"></script>--}}
+{{--<!-- Wow JavaScript -->--}}
+{{--<script src="{{asset('js/wow.min.js')}}"></script>--}}
+{{--<!-- Apexcharts JavaScript -->--}}
+{{--<script src="{{asset('js/apexcharts.js')}}}"></script>--}}
+{{--<!-- Slick JavaScript -->--}}
+{{--<script src="{{asset('js/slick.min.js')}}"></script>--}}
+{{--<!-- Select2 JavaScript -->--}}
+{{--<script src="{{asset('js/select2.min.js')}}"></script>--}}
+{{--<!-- Owl Carousel JavaScript -->--}}
+{{--<script src="{{asset('js/owl.carousel.min.js')}}"></script>--}}
+{{--<!-- Magnific Popup JavaScript -->--}}
+{{--<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>--}}
+{{--<!-- Smooth Scrollbar JavaScript -->--}}
+{{--<script src="{{asset('js/smooth-scrollbar.js')}}"></script>--}}
+{{--<!-- lottie JavaScript -->--}}
+{{--<script src="{{asset('js/lottie.js')}}"></script>--}}
+{{--<!-- am core JavaScript -->--}}
+{{--<script src="{{asset('js/core.js')}}"></script>--}}
+{{--<!-- am charts JavaScript -->--}}
+{{--<script src="{{asset('js/charts.js')}}"></script>--}}
+{{--<!-- am animated JavaScript -->--}}
+{{--<script src="{{asset('js/animated.js')}}"></script>--}}
+{{--<!-- am kelly JavaScript -->--}}
+{{--<script src="{{asset('js/kelly.js')}}"></script>--}}
+{{--<!-- am maps JavaScript -->--}}
+{{--<script src="{{asset('js/maps.js')}}"></script>--}}
+{{--<!-- am worldLow JavaScript -->--}}
+{{--<script src="{{asset('js/worldLow.js')}}"></script>--}}
+{{--<!-- Raphael-min JavaScript -->--}}
+{{--<script src="{{asset('js/raphael-min.js')}}"></script>--}}
+{{--<!-- Morris JavaScript -->--}}
+{{--<script src="{{asset('js/morris.js')}}"></script>--}}
+{{--<!-- Morris min JavaScript -->--}}
+{{--<script src="{{asset('js/morris.min.js')}}"></script>--}}
+{{--<!-- Custom JavaScript -->--}}
+{{--<script src="{{asset('js/custom.js')}}"></script>--}}
+{{--_________________________________________________________--}}
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="{{asset('js/jquery.min.js')}}"></script>
-<script src="{{asset('js/popper.min.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap4.min.js"></script>
 <!-- Appear JavaScript -->
-<script src="{{asset('js/jquery.appear.js')}}"></script>
+<script src="js/jquery.appear.js"></script>
 <!-- Countdown JavaScript -->
-<script src="{{asset('js/countdown.min.js')}}"></script>
+<script src="js/countdown.min.js"></script>
 <!-- Counterup JavaScript -->
-<script src="{{asset('js/waypoints.min.js')}}"></script>
-<script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
 <!-- Wow JavaScript -->
-<script src="{{asset('js/wow.min.js')}}"></script>
+<script src="js/wow.min.js"></script>
 <!-- Apexcharts JavaScript -->
-<script src="{{asset('js/apexcharts.js')}}}"></script>
+<script src="js/apexcharts.js"></script>
 <!-- Slick JavaScript -->
-<script src="{{asset('js/slick.min.js')}}"></script>
+<script src="js/slick.min.js"></script>
 <!-- Select2 JavaScript -->
-<script src="{{asset('js/select2.min.js')}}"></script>
+<script src="js/select2.min.js"></script>
 <!-- Owl Carousel JavaScript -->
-<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<script src="js/owl.carousel.min.js"></script>
 <!-- Magnific Popup JavaScript -->
-<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
 <!-- Smooth Scrollbar JavaScript -->
-<script src="{{asset('js/smooth-scrollbar.js')}}"></script>
+<script src="js/smooth-scrollbar.js"></script>
 <!-- lottie JavaScript -->
-<script src="{{asset('js/lottie.js')}}"></script>
+<script src="js/lottie.js"></script>
 <!-- am core JavaScript -->
-<script src="{{asset('js/core.js')}}"></script>
+<script src="js/core.js"></script>
 <!-- am charts JavaScript -->
-<script src="{{asset('js/charts.js')}}"></script>
+<script src="js/charts.js"></script>
 <!-- am animated JavaScript -->
-<script src="{{asset('js/animated.js')}}"></script>
+<script src="js/animated.js"></script>
 <!-- am kelly JavaScript -->
-<script src="{{asset('js/kelly.js')}}"></script>
+<script src="js/kelly.js"></script>
 <!-- am maps JavaScript -->
-<script src="{{asset('js/maps.js')}}"></script>
+<script src="'js/maps.js"></script>
 <!-- am worldLow JavaScript -->
-<script src="{{asset('js/worldLow.js')}}"></script>
+<script src="js/worldLow.js"></script>
 <!-- Raphael-min JavaScript -->
-<script src="{{asset('js/raphael-min.js')}}"></script>
+<script src="js/raphael-min.js"></script>
 <!-- Morris JavaScript -->
-<script src="{{asset('js/morris.js')}}"></script>
+<script src="js/morris.js"></script>
 <!-- Morris min JavaScript -->
-<script src="{{asset('js/morris.min.js')}}"></script>
+<script src="js/morris.min.js"></script>
 <!-- Custom JavaScript -->
-<script src="{{asset('js/custom.js')}}"></script>
+<script src="js/custom.js"></script>
+_________________________________________________________
