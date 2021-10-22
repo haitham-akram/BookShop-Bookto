cd2e9309104adbcc940b75f6a6ded9c647b01c90
haitham abu lamdi <hayhtam14@gmail.com>
2021-10-22 03:03:45 +0300

    change in appStyle secure_asset

--- resources/views/includes/AppStyle.blade.php
+++ resources/views/includes/AppStyle.blade.php
@@ -1,20 +1,25 @@
 <!-- Favicon -->
 <link rel="shortcut icon" href= "{{ asset('images/favicon.ico') }}"/>
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+{{--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">--}}
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">--}}
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/bootstrap.min.css') }}">
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/dataTables.bootstrap4.min.css')}}">--}}
-<link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/dataTables.bootstrap4.min.css')}}">
+{{--<link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap4.min.css">--}}
 <!-- select Library css-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 <!-- Typography CSS -->
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/typography.css')}}">--}}
-<link rel="stylesheet" type="text/css" href="css/typography.css">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/typography.css')}}">
+{{--<link rel="stylesheet" type="text/css" href="css/typography.css">--}}
 <!-- Style CSS -->
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">--}}
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/style.css') }}">
+{{--<link rel="stylesheet" type="text/css" href="css/style.css">--}}
 <!-- Responsive CSS -->
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.css') }}">--}}
-<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/responsive.css') }}">
+{{--<link rel="stylesheet" type="text/css" href="css/responsive.css">--}}
 
 
